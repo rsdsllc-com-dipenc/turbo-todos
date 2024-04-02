c9cf25c6bfe93d8f64afeedf8ae47fe56e97bbc6
Dipen Chauhan <dipenc@rsdsllc.com>
2024-04-02 09:52:20 -0700

    added todo

---
+++ app/models/todo.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :integer          not null, primary key
+#  status     :integer          default(0)
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Todo < ApplicationRecord
+end
